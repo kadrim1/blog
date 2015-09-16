934c0ce891729c6d0653c0890561203dd69d21ed
kadrim1 <kadri.mihhailova@khk.ee>
2015-09-16 15:43:37 +0300

    Parandatud link 3. Tere maailm link [Delivers #103481702]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 <button onclick="alert('Tere Maailm!')">Tere Maailm!</button>
-<a href="https://www.khk.ee" onClick="alert('Tere Maailm!')">Tere Maailm!</a>
+<a href="http://www.khk.ee" onClick="alert('Tere Maailm!')">Tere Maailm!</a>
 
 </body>
 </html>
