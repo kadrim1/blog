7d14792a9a8e33d50488640e6f209bb7a37a22ed
kadrim1 <kadri.mihhailova@khk.ee>
2015-09-17 00:02:28 +0300

    7. jQuery taustavärvi muutmine [Delivers #103481854]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -15,12 +15,31 @@
 <!--<img id="cat" onclick=getElementById("cat").src="ASSETS/pictures/dog.jpg" src="ASSETS/pictures/cat.jpg">-->
 <img id="pic1" src="ASSETS/pictures/cat.jpg "/>
 
+<span class="bg_buttons">
+    <button id="button1">Red</button>
+    <button id="button2">Green</button>
+    <button id="button3">Blue</button>
+</span>
+
+
 <script>
     $(document).ready(function () {
         $('#pic1').click(function () {
             $(this).replaceWith('<img src="ASSETS/pictures/dog.jpg"/>');
         });
+        $('#button1').click(function () {
+            $('body').css('backgroundColor', 'red');
+        });
+        $('#button2').click(function () {
+            $('body').css('backgroundColor', 'green');
+        });
+        $('#button3').click(function () {
+            $('body').css('backgroundColor', 'blue');
+        });
     });
+
 </script>
+
+
 </body>
 </html>
