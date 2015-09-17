db71fcae3f00ab455cc09b03072683185bcf9c79
kadrim1 <kadri.mihhailova@khk.ee>
2015-09-17 18:48:31 +0300

    18. Objektid [Delivers #103595520]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -1,13 +1,22 @@
 <?php
-$isik = array(
-    "ees" => "Kadri",
-    "pere" => "Mihhailova",
-    "vanus" => 26,
-);
+//$isik = array(
+//    "ees" => "Kadri",
+//    "pere" => "Mihhailova",
+//    "vanus" => 26,
+// echo "<p>" . $isik['ees'] . " " . $isik['pere'] . " (" . $isik['vanus'] . ")</p>";);
+class Isik
+{
+    var $ees = "Kadri";
+    var $pere = "Mihhailova";
+    var $vanus = 26;
+    var $sugu = "naine";
+}
+
+$kadri = new Isik;
 
 $eesnimi_algab_vokaaliga = false;
 
-echo "<p>" . $isik['ees'] . " " . $isik['pere'] . " (" . $isik['vanus'] . ")</p>";
+echo "<p>" . $kadri->ees . " " . $kadri->pere . " (" . $kadri->vanus . ")</p>";
 
 if ($eesnimi_algab_vokaaliga) {
     echo "<p>Nimi algab vokaaliga</p>";
