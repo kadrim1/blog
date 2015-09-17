c4bf43ba35f19f8c0c508ca8feb2fe1db6cca9a9
kadrim1 <kadri.mihhailova@khk.ee>
2015-09-17 18:25:36 +0300

    17. Tingimused [Delivers #103595496]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -9,5 +9,9 @@ $eesnimi_algab_vokaaliga = false;
 
 echo "<p>" . $isik['ees'] . " " . $isik['pere'] . " (" . $isik['vanus'] . ")</p>";
 
+if ($eesnimi_algab_vokaaliga) {
+    echo "<p>Nimi algab vokaaliga</p>";
+};
+
 
 ?>
