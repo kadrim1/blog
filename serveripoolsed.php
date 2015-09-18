8a3a512eabbf4e5040f05dc3e05aadda3999ad7f
kadrim1 <kadri.mihhailova@khk.ee>
2015-09-18 22:12:58 +0300

    Parandatud 18. Objektid [Delivers #103595520]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -6,18 +6,25 @@
 // echo "<p>" . $isik['ees'] . " " . $isik['pere'] . " (" . $isik['vanus'] . ")</p>";);
 class Isik
 {
-    var $ees = "Kadri";
-    var $pere = "Mihhailova";
-    var $vanus = 26;
-    var $sugu = "naine";
+    public $persoon = array("ees" => "", "pere" => "", "vanus" => 0, "sugu" => "");
+
+    function Isik($ees, $pere, $vanus, $sugu)
+    {
+
+        $this->persoon["ees"] = $ees;
+        $this->persoon["pere"] = $pere;
+        $this->persoon["vanus"] = $vanus;
+        $this->persoon["sugu"] = $sugu;
+
+    }
 }
 
-$person = new Isik;
+$person = new Isik("Kadri", "Mihhailova", 26, "naine");
 
-echo "<p>" . $person->ees . " " . $person->pere . " (" . $person->vanus . ")</p>";
+echo "<p>" . $person->persoon["ees"] . " " . $person->persoon["pere"] . " (" . $person->persoon["vanus"] . ")</p>";
 
 $vowels = array('A', 'E', 'I', 'O', 'U', 'Õ', 'Ä', 'Ö', 'Ü', 'a', 'e', 'i', 'o', 'u', 'õ', 'ä', 'ö', 'ü');
-if (in_array(substr($person->ees, 0, 1), $vowels)) {
+if (in_array(substr($person->persoon["ees"], 0, 1), $vowels)) {
     $eesnimi_algab_vokaaliga = true;
 } else {
     $eesnimi_algab_vokaaliga = false;
