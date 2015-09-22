1abf469cbe862079a2b8767823d265f8bd2866b8
kadrim1 <kadri.mihhailova@khk.ee>
2015-09-22 18:49:58 +0300

    28. Liigsete korduvate andmeplokkide eemaldamine [Delivers #103880058]

--- index.php
+++ index.php
@@ -103,6 +103,7 @@
                     <h3 class="post-subtitle">
                         Problems look mighty small from 150 miles up
                     </h3>
+
                 </a>
 
                 <p class="post-meta">Posted by <a href="#">Start Bootstrap</a> on September 24, 2014</p>
