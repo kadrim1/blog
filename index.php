1a0ca481e4a436078b33a2cdaa94111350dff676
kadrim1 <kadri.mihhailova@khk.ee>
2015-09-23 11:09:06 +0300

    28. Fake it until you make it [Delivers #103880070]

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+$posts = array(
+    'title' => 'Man must explore, and this is exploration at its greatest',
+    'subtitle' => 'Problems look mighty small from 150 miles up',
+    'author' => 'Start Bootstrap',
+    'date' => 'September 24, 2014',
+);
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -97,16 +108,17 @@
             <div class="post-preview">
                 <a href="post.html">
                     <h2 class="post-title">
-                        Man must explore, and this is exploration at its greatest
+                        <?php echo $posts['title'] ?>
                     </h2>
 
                     <h3 class="post-subtitle">
-                        Problems look mighty small from 150 miles up
+                        <?php echo $posts['subtitle'] ?>
                     </h3>
 
                 </a>
 
-                <p class="post-meta">Posted by <a href="#">Start Bootstrap</a> on September 24, 2014</p>
+                <p class="post-meta">Posted by <a
+                        href="#"><?php echo $posts['author'] ?></a> <?php echo $posts['date'] ?></p>
             </div>
             <hr>
 
